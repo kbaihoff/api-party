d714f5b727ddf43e700d2105634ad9646e5e9018
Kai Hoffman <hoffm154@purdue.edu>
2017-06-20 11:39:24 -0400

    Format Github component

--- src/Github.js
+++ src/Github.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 class Github extends React.Component {
   render() {
-    return <h1>Github Component</h1>
+    return (
+      <div className="github">
+        <img src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github logo" className="github-logo" />
+        <form>
+          <div>
+            <input type="text" />
+          </div>
+          <div>
+            <button type="submit">Look up Github user</button>
+          </div>
+        </form>
+      </div>
+    )
   }
 }
 
